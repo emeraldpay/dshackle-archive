ff60392fce3e25722bb33abb65931fe7110e967b
Igor Artamonov <igor@artamonov.ru>
2022-11-09 22:03:00 -0500

    rel: [e0dc3c5a85aca08966574ec0c21fa656377f99dd]

--- src/test/groovy/io/emeraldpay/dshackle/archive/storage/fs/FilesStorageAccessSpec.groovy
+++ src/test/groovy/io/emeraldpay/dshackle/archive/storage/fs/FilesStorageAccessSpec.groovy
@@ -69,7 +69,7 @@ class FilesStorageAccessSpec extends Specification {
     }
 
 
-    def "Scan files in different palces"() {
+    def "Scan files in different places"() {
         setup:
         def dir = Files.createTempDirectory("dshackle-fs-test")
         def subdir1 = dir.resolve("subdir1")
@@ -92,5 +92,37 @@ class FilesStorageAccessSpec extends Specification {
                 "subdir1/test1.txt", "subdir1/test2.txt",
                 "subdir2/test3.txt", "subdir2/test4.txt"
         ].toSorted()
+    }
+
+    def "Scan files with actual limit"() {
+        setup:
+        def dir = Files.createTempDirectory("dshackle-fs-test")
+        def subdir = dir.resolve("subdir")
+        Files.createDirectory(subdir)
+        (10..99).forEach { i ->
+            Files.write(subdir.resolve("test${i}.txt"), i.toString().getBytes())
+        }
+
+        def publisher = new FilesStorageAccess.FilesPublisher(dir)
+        when:
+        def act = Flux.from(publisher)
+                .filter {
+                    def i = it.fileName.toString().substring(4, 6).toInteger()
+                    println("++++ i = $i / ${it.fileName.toString()} - ${it.fileName.toString().substring(4, 6)}")
+                    i < 15
+                }
+                .doOnNext {
+                    println("!!!!!!!!")
+                    println("!!!!!!!! ACCEPT $it")
+                    println("!!!!!!!!")
+                }
+                .doOnError { t ->
+                    t.printStackTrace()
+                }
+                .collectList().block(Duration.ofSeconds(1))
+        then:
+        act.collect {dir.relativize(it).toString() }.toSorted() == [
+                "subdir/test10.txt", "subdir/test11.txt", "subdir/test12.txt", "subdir/test13.txt", "subdir/test14.txt"
+        ]
     }
 }
