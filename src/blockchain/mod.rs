44d47e1d755d066b38d9ebf6d626b37c0728da34
Igor Artamonov <igor@artamonov.ru>
2025-01-22 15:30:08 +0000

    solution: works with bitcoin

--- src/blockchain/mod.rs
+++ src/blockchain/mod.rs
@@ -1,45 +1,146 @@
 pub mod ethereum;
 pub mod connection;
+pub mod bitcoin;
 
-use alloy::primitives::{BlockHash, TxHash};
-use alloy::rpc::types::Block;
+use std::str::FromStr;
 use apache_avro::types::Record;
 use async_trait::async_trait;
-use num_traits::cast::ToPrimitive;
-use connection::TransactionId;
-use anyhow::Result;
+use anyhow::{anyhow, Error, Result};
+use crate::blockchain::bitcoin::BitcoinData;
+use crate::blockchain::connection::Blockchain;
+use crate::blockchain::ethereum::EthereumData;
 
-pub fn to_zero_hex<T: AsRef<[u8]>>(value: T) -> String {
-    format!("0x{}", hex::encode(value))
+///
+/// Defined the data types for a blockchain
+pub trait BlockchainTypes: Send + Sync + Sized {
+
+    ///
+    /// Type of the Block Hash / Block Identifier
+    type BlockHash: FromStr + Send + Sync;
+
+    ///
+    /// Type of the Transaction Hash / Transaction Identifier
+    type TxId: Send + Sync;
+
+    ///
+    /// Block details converted from the JSON response
+    type BlockParsed: Send + Sync;
+
+    ///
+    /// Data provider for the blockchain
+    type DataProvider: BlockchainData<Self> + Send + Sync + Sized;
+
+    fn create_data_provider(blockchain: Blockchain, id: String) -> Self::DataProvider;
 }
 
-pub fn to_zero_number<T: ToPrimitive>(value: T) -> String {
-    format!("0x{}", hex::encode(value.to_u64().unwrap_or_default().to_be_bytes()))
+pub enum BlockchainType {
+    Ethereum,
+    Bitcoin,
 }
 
-pub enum BlockReference {
-    Hash(BlockHash),
+pub struct EthereumType {}
+impl BlockchainTypes for EthereumType {
+    type BlockHash = alloy::primitives::BlockHash;
+    type TxId = alloy::primitives::TxHash;
+    type BlockParsed = alloy::rpc::types::Block<Self::TxId>;
+
+    type DataProvider = EthereumData;
+
+    fn create_data_provider(blockchain: Blockchain, id: String) -> Self::DataProvider {
+        EthereumData::new(blockchain, id)
+    }
+}
+pub struct BitcoinType {}
+impl BlockchainTypes for BitcoinType {
+    type BlockHash = bitcoin::BlockHash;
+    type TxId = bitcoin::TxHash;
+    type BlockParsed = bitcoin::BitcoinBlock;
+
+    type DataProvider = BitcoinData;
+
+    fn create_data_provider(blockchain: Blockchain, id: String) -> Self::DataProvider {
+        BitcoinData::new(blockchain, id)
+    }
+}
+
+///
+/// Data provider for the blockchain
+#[async_trait]
+pub trait BlockchainData<T: BlockchainTypes>: Send + Sync {
+    ///
+    /// Actual blockchain id, as specified by the user, for the field of `blockchainId` in the output records.
+    /// Ex for the blockchain "Ethereum Classic" the user may set the id as "ETC" and for "Ethereum" as "ETH",
+    /// while the both blockchain are of the type "Ethereum" and share the same code/types/etc except this id
+    fn blockchain_id(&self) -> String;
+
+    ///
+    /// Get the details for the block
+    async fn fetch_block(&self, height: &BlockReference<T::BlockHash>) -> Result<(Record, T::BlockParsed, Vec<T::TxId>)>;
+
+    ///
+    /// Get the details for the transaction
+    async fn fetch_tx(&self, block: &T::BlockParsed, index: usize) -> Result<Record>;
+}
+
+///
+/// A reference to a block
+pub enum BlockReference<T> where T: FromStr {
+    /// By its hash / identifier
+    Hash(T),
+    /// Byt its height
     Height(u64),
 }
 
-impl From<BlockHash> for BlockReference {
-    fn from(value: BlockHash) -> Self {
-        BlockReference::Hash(value)
+impl<T> FromStr for BlockReference<T> where T: FromStr {
+    type Err = Error;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        let inner = T::from_str(s)
+            .map_err(|_| anyhow!("Failed to convert from string. Value {}", s))?;
+        Ok(BlockReference::Hash(inner))
     }
 }
 
-impl From<u64> for BlockReference {
-    fn from(value: u64) -> Self {
-        BlockReference::Height(value)
+impl<T> BlockReference<T> where T: FromStr {
+
+    pub fn hash(h: T) -> Self {
+        BlockReference::Hash(h)
+    }
+
+    pub fn height(h: u64) -> Self {
+        BlockReference::Height(h)
+    }
+
+}
+
+pub struct JsonString(String);
+
+impl TryFrom<Vec<u8>> for JsonString {
+    type Error = Error;
+
+    fn try_from(value: Vec<u8>) -> std::result::Result<Self, Self::Error> {
+        if value == b"null" {
+            return Err(anyhow!("Null value"));
+        }
+        let str = String::from_utf8_lossy(&value[1..(value.len() - 1)]).to_string();
+        Ok(JsonString(str))
     }
 }
 
-#[async_trait]
-pub trait BlockchainData: Send + Sync {
+pub enum OptionalJson<T> {
+    Some(T),
+    None,
+}
 
-    fn blockchain_id(&self) -> String;
+impl<T> TryFrom<Vec<u8>> for OptionalJson<T> where T: TryFrom<Vec<u8>> {
+    type Error = Error;
 
-    async fn fetch_block(&self, height: &BlockReference) -> Result<(Record, Block<TxHash>, Vec<TransactionId>)>;
-
-    async fn fetch_tx(&self, block: &Block<TxHash>, index: usize) -> Result<Record>;
+    fn try_from(value: Vec<u8>) -> std::result::Result<Self, Self::Error> {
+        if value == b"null" {
+            return Ok(OptionalJson::None);
+        }
+        let value = T::try_from(value)
+            .map_err(|_| anyhow!("Failed to convert from bytes"))?;
+        Ok(OptionalJson::Some(value))
+    }
 }
